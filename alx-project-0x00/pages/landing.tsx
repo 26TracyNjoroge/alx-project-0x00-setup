c3dbc778c4ea60f2b6644d4d4bbc9dcce80f3557
Tracy Njoroge <tracywangari997@gmail.com>
2025-05-29 00:42:18 +0300

    Update button interface

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,7 +7,8 @@ const Landing: React.FC = () => {
             <h1 className=" text-xl font-extralight">Landing Page</h1>
             <Card />
             <Card />
-            <Button styles="rounded-full" width="w-full" title="Rounded Large" />
+            <Button styles="rounded-full" width="w-full" title="Rounded Full" />
+            <Button styles="rounded-lg" width="w-lg" title="Rounded Large" />
             <Button styles="rounded-md" width="w-md" title="Rounded Medium" />
             <Button styles="rounded-sm"  width="w-sm" title="Rounded Small" />
 
